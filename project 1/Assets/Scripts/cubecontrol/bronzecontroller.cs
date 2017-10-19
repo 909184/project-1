dddbe9b2b16fb0a439a4f02362dd20c39b0fd0e2
MIHASHI\hime <alw17@hampshire.edu>
2017-10-19 18:43:43 -0400

    final

---
+++ project 1/Assets/Scripts/cubecontrol/bronzecontroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bronzecontroller : MonoBehaviour {
+
+	void OnMouseDown() {
+		gamecontroller.playerScore += gamecontroller.bronzeScore;
+		gamecontroller.bronzeOre++;
+		Destroy (gameObject);
+
+		gamecontroller.bronzeSupply--;
+	}
+}
---
+++ project 1/Assets/Scripts/cubecontrol/silvercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class silvercontroller : MonoBehaviour {
+	
+	void OnMouseDown() {
+		gamecontroller.playerScore += gamecontroller.silverScore;
+		gamecontroller.silverOre++;
+		Destroy (gameObject);
+
+		gamecontroller.silverSupply--;
+
+	}
+
+}
+
+
