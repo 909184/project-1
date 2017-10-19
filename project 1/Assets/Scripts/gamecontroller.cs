482858313479ad9a339815243afcae4ab2b2efef
MIHASHI\hime <alw17@hampshire.edu>
2017-10-19 18:51:41 -0400

    final

--- project 1/Assets/Scripts/gamecontroller.cs
+++ project 1/Assets/Scripts/gamecontroller.cs
@@ -7,15 +7,25 @@ public class gamecontroller : MonoBehaviour {
 	int startMining;
 	public int miningSpeed;
 
-	int bronzeOre;
-	int silverOre;
-	public int bronzeSupply;
-	public int silverSupply;
+	public static int bronzeOre;
+	public static int silverOre;
+	public static int goldOre;
+	public static int bronzeSupply;
+	public static int silverSupply;
+	public static int goldSupply;
+
+	public static int bronzeScore;
+	public static int silverScore;
+	public static int goldScore;
+	public static int playerScore;
 
 	GameObject bronzeCube;
 	GameObject silverCube;
+	GameObject goldCube;
 
-	public GameObject cubePrefab;
+	public GameObject bronzePrefab;
+	public GameObject silverPrefab;
+	public GameObject goldPrefab;
 	Vector3 cubePostion;
 
 
@@ -27,9 +37,15 @@ public class gamecontroller : MonoBehaviour {
 		miningSpeed = 3;
 		bronzeOre = 0;
 		silverOre = 0;
-		bronzeSupply = 3;
-		silverSupply = 2;
+		goldOre = 0;
+		bronzeSupply = 0;
+		silverSupply = 0;
+		goldSupply = 0;
 
+		bronzeScore = 1;
+		silverScore = 10;
+		goldScore = 100;
+		playerScore = 0;
 
 	}
 	
@@ -37,32 +53,47 @@ public class gamecontroller : MonoBehaviour {
 	void Update () {
 		if (Time.time > startMining) {
 
-			if (bronzeSupply >= 1) {
-				bronzeSupply -= 1;
-				bronzeOre += 1;
+
+		//spawn silver ore
+			if (bronzeSupply >= 4) {
 
 				cubePostion = new Vector3 (Random.Range (-7, 8), Random.Range (-7, 8), Random.Range (-7, 8));
-				bronzeCube = Instantiate (cubePrefab, cubePostion, Quaternion.identity);
+				silverCube = Instantiate (silverPrefab, cubePostion, Quaternion.identity);
 
-				bronzeCube.GetComponent<Renderer> ().material.color = Color.red;
-			} 
-			else if (silverSupply >= 1 && bronzeSupply == 0) {
-				silverSupply -= 1;
-				silverOre += 1;
+				silverCube.GetComponent<Renderer> ().material.color = Color.white;
 
-				cubePostion = new Vector3 (Random.Range (-7, 8), Random.Range (-7, 8), Random.Range (-7, 8));
-				silverCube = Instantiate (cubePrefab, cubePostion, Quaternion.identity);
-
-				silverCube.GetComponent<Renderer> ().material.color = Color.white; 
+				silverSupply += 1;
 			}
 
-			print ("Bronze:" + bronzeOre + "    Silver:" + silverOre);
+			//spawn gold ore
+			else if (bronzeSupply == 2 && silverSupply == 2) {
+				cubePostion = new Vector3 (Random.Range (-7, 8), Random.Range (-7, 8), Random.Range (-7, 8));
+				goldCube = Instantiate (goldPrefab, cubePostion, Quaternion.identity);
 
-			startMining += miningSpeed;
+				goldCube.GetComponent<Renderer> ().material.color = Color.yellow;
 
+				goldSupply += 1;
+
+			}
+
+		//spawn bronze ore
+			if (bronzeSupply < 4) {
+				bronzeSupply += 1;
+
+
+				cubePostion = new Vector3 (Random.Range (-7, 8), Random.Range (-7, 8), Random.Range (-7, 8));
+				bronzeCube = Instantiate (bronzePrefab, cubePostion, Quaternion.identity);
+
+				bronzeCube.GetComponent<Renderer> ().material.color = Color.red;
+
+			}
+
+				print ("Bronze:" + bronzeOre + "    Silver:" + silverOre + "    Gold:" + goldOre);
+
+				startMining += miningSpeed;
 				
+			print ("Score:" + playerScore);
 		}
 	}
 }
-
 
